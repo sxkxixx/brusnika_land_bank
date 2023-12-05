8e101ada72b351f4ba5b0c49a20bf8889985b74c
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-12-05 21:11:24 +0500

    В попытках правильно написать докерфайлы

--- land_bank/run_application.sh
+++ land_bank/run_application.sh
@@ -3,4 +3,4 @@
 alembic revision --autogenerate -m 'initial'
 alembic upgrade 07ee6db86693
 
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
