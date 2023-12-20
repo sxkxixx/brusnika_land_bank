fb999dc828ce98ba2476306d6e7a79b96e062ffd
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-12-20 16:52:20 +0500

    alembic migration in docker

--- land_bank/run_application.sh
+++ land_bank/run_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alembic revision --autogenerate -m 'initial'
-alembic upgrade head
+#alembic revision --autogenerate -m 'initial'
+alembic upgrade aba570fb9950
 
 gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
