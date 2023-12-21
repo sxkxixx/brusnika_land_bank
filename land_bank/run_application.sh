f5b0fdf86106af22fbab7e1d1edeab3dde1e168b
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-12-21 13:42:18 +0500

    docker+alembic

--- land_bank/run_application.sh
+++ land_bank/run_application.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#alembic revision --autogenerate -m 'initial'
-alembic upgrade aba570fb9950
+chmod +x ./revision.sh
 
 gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
